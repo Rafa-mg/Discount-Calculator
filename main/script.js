1dcd703d2f9c1795bea366df5d8a77c28c84c287
Rafael <mg.rafaeld@gmail.com>
2023-12-29 12:25:27 -0300

    avances pero me encontre un bug

--- main/script.js
+++ main/script.js
@@ -1,1 +1,18 @@
-console.log('Hola, mundo!')
+let resultado = document.querySelector('#resultado');
+let precioInicial = document.querySelector('#input1');
+let descuentoInput = document.querySelector('#input2');
+
+const calculo = document.querySelector('#finalPrice');
+
+calculo.addEventListener('click', calcular);
+
+function calcular() {
+    let precioInicialConvertido = Number(precioInicial.value);
+    let descuentoConvertido = Number(descuentoInput.value);
+
+    let discount = (precioInicialConvertido * descuentoConvertido) / 100;
+
+    let precioFinal = precioInicialConvertido - discount;
+
+    resultado.value = `El precio final es ${precioFinal}`;
+}
